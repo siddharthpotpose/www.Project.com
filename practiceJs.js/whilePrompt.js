fcf408c8a945c6d1e9ff47c6eb4c5129cf807115
siddharthpotpose <siddharthpotpose9@gmail.com>
2022-10-20 15:03:20 +0530

    new practice

--- practiceJs.js/whilePrompt.js
+++ practiceJs.js/whilePrompt.js
@@ -1,15 +1,38 @@
-let num="yes";
-// ----------------------------------------------------
-while(num==="yes"){
-    num=prompt("do you want continue yes/no")
-    console.log("good")
+//for loop with prompt
+for(num=1;num<=100;num++){
+    num=prompt("enter below 99 number it is repeated")
+    console.log("number ="+num)
 }
+// -------------------------------------------------------
+// while loop with prompt 
+let order="yes";
+while(order==="yes"){
+    order=prompt("do you want repeate this order yes/no")
+    console.log("order="+order)
+}
+// ----------------------------------------------------------------
+// do while loop with prompt 
+let print="yes";
+do {
+    print=prompt("do you want print yes/no")
+    console.log("print number="+print)
+}while(print==="yes")
 
 
 
-// =====================================================
+
+//let num="yes";
+// // ----------------------------------------------------
+// while(num==="yes"){
+//     num=prompt("do you want continue yes/no")
+//     console.log("good")
+// }
+
+
+
+// // =====================================================
 // do{
 //     num=prompt("do you want continue yes/no")
-//     console.log("result")
+//     console.log("result"+num)
    
-// }while(num==="yes")
+// }while(num==="no")
