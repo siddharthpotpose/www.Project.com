5e446f04d6ca8fa2bda2f49eb8f849dadc2f738f
siddharthpotpose <siddharthpotpose9@gmail.com>
2022-11-25 10:39:18 +0530

    new electric city unit else if ladder added

---
+++ javaScript/electricUnit.js
@@ -0,0 +1,27 @@
+
+let unit=prompt("calculating your electricity units. pls enter your electricity units ");
+if(unit>100){
+    document.write("you ara consumed "+unit+" units and per unit charge is 15 Rs/per unit. pay total bill is =",unit*15)
+}
+else if(unit>80){
+    document.write("you are consumed "+unit+" units and per unit charges is 13 Rs/per unit. pay total bill is = ",unit*13)
+}
+else if(unit>60){
+    document.write("you are consumed "+unit+" units and per unit charge is 11 Rs/per unit. pay total bill = ",unit*11)
+}
+else if(unit>40){
+    document.write("you are consumed "+ unit+" units and per unit charge is 10 Rs/per unit. pay total bill = ",unit*10)
+}
+else if(unit>20){
+    document.write("you are consumed "+unit+" units and per unit charge is 8 Rs/per unit. pay total bill = ",unit*8)
+}
+else if(unit>10){
+    document.write("you are consumed "+unit+" units and per unit charges is 5 Rs/per unit. pay total bill = ",unit*5)
+}
+else if(unit>=1){
+    document.write("you are consumed "+unit+" units and per unit charge is 3 Rs/per unit. pay total bill = ",unit*3)
+}
+else {
+    document.write("wow you are consumed 0 units. your electric bill is 0 ")
+}
+
