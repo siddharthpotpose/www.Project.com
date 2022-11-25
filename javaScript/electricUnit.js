fe721d35e9be046edac33e0950e59c6a0fe49f91
siddharthpotpose <siddharthpotpose9@gmail.com>
2022-11-25 10:52:49 +0530

    new added

--- javaScript/electricUnit.js
+++ javaScript/electricUnit.js
@@ -1,25 +1,25 @@
 
 let unit=prompt("calculating your electricity units. pls enter your electricity units ");
 if(unit>100){
-    document.write("you ara consumed "+unit+" units and per unit charge is 15 Rs/per unit. pay total bill is =",unit*15 +"Rs")
+    document.write("you ara consumed "+unit+" units and per unit charge is 15 Rs/per unit. pay total bill is =",unit*15 +"/Rs")
 }
 else if(unit>80){
-    document.write("you are consumed "+unit+" units and per unit charges is 13 Rs/per unit. pay total bill is = ",unit*13 +"Rs")
+    document.write("you are consumed "+unit+" units and per unit charges is 13 Rs/per unit. pay total bill is = ",unit*13 +"/Rs")
 }
 else if(unit>60){
-    document.write("you are consumed "+unit+" units and per unit charge is 11 Rs/per unit. pay total bill = ",unit*11 +"Rs")
+    document.write("you are consumed "+unit+" units and per unit charge is 11 Rs/per unit. pay total bill = ",unit*11 +"/Rs")
 }
 else if(unit>40){
-    document.write("you are consumed "+ unit+" units and per unit charge is 10 Rs/per unit. pay total bill = ",unit*10 +"Rs")
+    document.write("you are consumed "+ unit+" units and per unit charge is 10 Rs/per unit. pay total bill = ",unit*10 +"/Rs")
 }
 else if(unit>20){
-    document.write("you are consumed "+unit+" units and per unit charge is 8 Rs/per unit. pay total bill = ",unit*8 +"Rs")
+    document.write("you are consumed "+unit+" units and per unit charge is 8 Rs/per unit. pay total bill = ",unit*8 +"/Rs")
 }
 else if(unit>10){
-    document.write("you are consumed "+unit+" units and per unit charges is 5 Rs/per unit. pay total bill = ",unit*5 +"Rs")
+    document.write("you are consumed "+unit+" units and per unit charges is 5 Rs/per unit. pay total bill = ",unit*5 +"/Rs")
 }
 else if(unit>=1){
-    document.write("you are consumed "+unit+" units and per unit charge is 3 Rs/per unit. pay total bill = ",unit*3 +"Rs")
+    document.write("you are consumed "+unit+" units and per unit charge is 3 Rs/per unit. pay total bill = ",unit*3 +"/Rs")
 }
 else {
     document.write("wow you are consumed 0 units. your electric bill is 0 ")
